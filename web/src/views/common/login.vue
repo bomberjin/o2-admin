86ba928e7ff6eb94d95ffe2a7fe0fa89b4a9f469
bomber <bomber_jin@163.com>
2021-05-13 20:05:39 +0800

    fix bug

--- web/src/views/common/login.vue
+++ web/src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">O2-admin </h2>
+          <p class="brand-info__intro">O2-admin基于vue、element-ui构建开发，简单易懂，通用性强</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
